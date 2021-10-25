32ae53b4ac75a947720b07453d31b31a9f351cde
Arthur Kirsanov <kirsappsinc@gmail.com>
2021-10-25 22:45:07 +0300

    added documentation

--- lib/builder.dart
+++ lib/builder.dart
@@ -5,9 +5,10 @@ import 'package:source_gen/source_gen.dart';
 
 export 'src/annotations.dart';
 
-/// Builds generators for `build_runner` to run
+/// Builds generator for Equatable props generation
 Builder propsGenerator(BuilderOptions options) =>
     SharedPartBuilder([PropsGenerator()], 'props');
 
+/// Builds generator for copyWith and copyWithNull methods generation
 Builder copyWithGenerator(BuilderOptions options) =>
     SharedPartBuilder([CopyWithGenerator()], 'copy_with');
