f9abbb33219170da9a3e45fab6c20ec85018c9db
Arthur Kirsanov <kirsappsinc@gmail.com>
2021-10-27 22:11:33 +0300

    added example

---
+++ example/bin/example.dart
@@ -0,0 +1,59 @@
+import 'package:boilerplate_annotations/boilerplate_annotations.dart';
+import 'package:equatable/equatable.dart';
+
+part 'example.g.dart';
+
+@props
+@copyWith
+class Payment extends Equatable {
+  final int id;
+  final String? description;
+  final Customer? customer;
+  const Payment({required this.id, this.customer, this.description});
+
+  @override
+  List<Object?> get props => _$PaymentProps(this);
+}
+
+@props
+@copyWith
+class Customer extends Equatable {
+  final int id;
+  final String name;
+  final String surname;
+  final String? patronymic;
+  final Address? address;
+  const Customer({
+    required this.id,
+    required this.name,
+    required this.surname,
+    this.patronymic,
+    this.address,
+  });
+
+  @override
+  List<Object?> get props => _$CustomerProps(this);
+}
+
+@props
+@copyWith
+class Address extends Equatable {
+  final String? street;
+  final int? home;
+  const Address({this.street, this.home});
+
+  @override
+  List<Object?> get props => _$AddressProps(this);
+}
+
+void main(List<String> arguments) {
+  const payment = Payment(
+    id: 4,
+    description: 'test',
+    customer: Customer(id: 1, name: 'John', surname: 'Dou', patronymic: 'test'),
+  );
+
+  payment.copyWith(
+    description: null,
+  ); // == Payment(id: 4,description: 'test',customer:Customer(id: 1, name: 'John', surname: 'Dou', patronymic: 'test'),),),);
+}
